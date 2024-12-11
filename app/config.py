438a3cf786d7096d7a7c82be980ae29386fc72c2
FYNJYserty <flipper02003@gmail.com>
2024-12-11 21:10:17 +0300

    Добавлены первые шаблоны и подключение базы данных

--- app/config.py
+++ app/config.py
@@ -6,3 +6,7 @@ load_dotenv()
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'abcde'
     DB_SERVER = os.environ.get('DB_SERVER') or 'localhost'
+    DB_USER = os.environ.get('DB_USER') or 'postgres'
+    DB_PASSWORD = os.environ.get('DB_PASSWORD') or 'your_password'
+    DB_NAME = os.environ.get('DB_NAME') or 'your_db_name'
+    DB_PORT = os.environ.get('DB_PORT') or 'your_db_port'
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,23 @@
+from app.forms import RegistrationForm, EditUserForm
 from app import app
+from flask import render_template, redirect, flash, url_for, request
+import psycopg
 
+@app.route('/index')
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
+def index():
+    return render_template('index.html')
+
+def test_connection():
+    con = None
+    try:
+        con = psycopg.connect(host=app.config['DB_SERVER'],
+                                user=app.config['DB_USER'],
+                                password=app.config['DB_PASSWORD'],
+                                dbname=app.config['DB_NAME'])
+    except Exception as e:
+        print(f"Ошибка подключения: {e}")
+    else:
+        print(f"Подключение успешно: {con}")
+    finally:
+        return con
